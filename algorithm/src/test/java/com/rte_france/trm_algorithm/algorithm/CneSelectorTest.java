5e680cebfd5689908586a07de4a1eee0625031bc
SCHINDLER Hugo <hugo.schindler@rte-france.com>
2024-05-17 16:13:03 +0200

    Add CNE selector

---
+++ algorithm/src/test/java/com/rte_france/trm_algorithm/algorithm/CneSelectorTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.rte_france.trm_algorithm.algorithm;
+
+import com.powsybl.iidm.network.Branch;
+import com.powsybl.iidm.network.Network;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Hugo Schindler {@literal <hugo.schindler at rte-france.com>}
+ * @author Viktor Terrier {@literal <viktor.terrier at rte-france.com>}
+ */
+class CneSelectorTest {
+    @Test
+    void testOneInterconnectionCneSelector() {
+        String networkResourcePath = "NETWORK_SINGLE_LOAD_TWO_GENERATORS_WITH_COUNTRIES.uct";
+        String networkName = Paths.get(networkResourcePath).getFileName().toString();
+        Network network = Network.read(networkName, CneSelectorTest.class.getResourceAsStream(networkResourcePath));
+
+        Set<Branch> branchList = CneSelector.getNetworkElements(network);
+        assertEquals(1, branchList.size());
+        assertTrue(branchList.contains(network.getBranch("FGEN1 11 BLOAD 11 1")));
+    }
+}
---
+++ doc/algorithm.md
@@ -0,0 +1,10 @@
+# Transfert Reliability Margin (TRM) algorithm
+
+This document describes the current state of the algorithm.
+
+## Critical Network Element (CNE) selection and filtering
+
+### CNE selection
+
+All the interconnections are considered to be CNEs.
+> This simple behavior might change later. 
