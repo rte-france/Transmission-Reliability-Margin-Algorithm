a7b822a6b746dcf6610d00e8e85ce257cf6c806d
huaracaseb <sebastian.huaraca.lapa@gmail.com>
2025-04-24 11:39:34 +0200

    Test Multi Lines elementName

--- src/main/java/com/rte_france/trm_algorithm/UcteMapping.java
+++ src/main/java/com/rte_france/trm_algorithm/UcteMapping.java
@@ -11,7 +11,7 @@ import com.powsybl.iidm.network.Line;
 import com.powsybl.iidm.network.Network;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.ArrayList;
+
 import java.util.List;
 
 /**
@@ -27,25 +27,49 @@ public final class UcteMapping {
         String voltageLevelSide2 = getVoltageLevelSide2(marketBasedId);
         String orderCode = getOrderCode(marketBasedId);
 
-        List<Line> matchLine = new ArrayList<>();
-        networkReference.getLines().forEach(line -> {
-            String idLine = line.getId();
+        List<Line> matchLine = networkReference.getLineStream().toList();
+        matchLine = matchLine.stream().filter(n -> getVoltageLevelSide1(n.getId()).equals(voltageLevelSide1)).toList();
+        matchLine = matchLine.stream().filter(n -> getVoltageLevelSide2(n.getId()).equals(voltageLevelSide2)).toList();
+        matchLine = matchLine.stream().filter(n -> getOrderCode(n.getId()).equals(orderCode)).toList();
+        return createMappingResults(marketBasedId, matchLine);
+    }
 
-            if (getVoltageLevelSide1(idLine).equals(voltageLevelSide1) && getVoltageLevelSide2(idLine).equals(voltageLevelSide2) && getOrderCode(idLine).equals(orderCode)) {
-                matchLine.add(line);
+    public static List<MappingResults> mapNetworks(Network networkReference, Network networkMarketBased, List<String> marketBasedId) {
+        return marketBasedId.stream().map(id -> {
+
+            String voltageLevelSide1 = getVoltageLevelSide1(id);
+            String voltageLevelSide2 = getVoltageLevelSide2(id);
+            String orderCode = getOrderCode(id);
+            String elementName = networkMarketBased.getLine(id).getProperty("elementName");
+            String elementName1 = elementName.substring(0, 4);
+            String elementName2 = elementName.substring(8);
+
+            List<Line> matchLine = networkReference.getLineStream()
+                    .filter(n -> getVoltageLevelSide1(n.getId()).equals(voltageLevelSide1))
+                    .filter(n -> getVoltageLevelSide2(n.getId()).equals(voltageLevelSide2))
+                    .filter(n -> getOrderCode(n.getId()).equals(orderCode)).toList();
+            if (matchLine.isEmpty()) {
+                matchLine = networkReference.getLineStream()
+                        .filter(n -> getVoltageLevelSide1(n.getId()).equals(voltageLevelSide1))
+                        .filter(n -> getVoltageLevelSide2(n.getId()).equals(voltageLevelSide2))
+                        .filter(n -> n.getProperty("elementName").substring(0, 4).equals(elementName1)
+                                && n.getProperty("elementName").substring(8).equals(elementName2)).toList();
             }
-
-        });
-        int nombreLines = matchLine.size();
-
-        if (nombreLines > 1) {
-            LOGGER.error("Several matching lines found for: {}", marketBasedId);
-        } else if (nombreLines == 0) {
-            LOGGER.error("No matching line found for: {}", marketBasedId);
-        } else {
-            return new MappingResults(marketBasedId, matchLine.get(0).getId(), true);
+            return createMappingResults(id, matchLine);
         }
-        return new MappingResults(marketBasedId, "", false);
+        ).toList();
+    }
+
+    private static MappingResults createMappingResults(String id, List<Line> matchLine) {
+        int nombreLines = matchLine.size();
+        if (nombreLines > 1) {
+            LOGGER.error("Several matching lines found for: {}", id);
+        } else if (nombreLines == 0) {
+            LOGGER.error("No matching line found for: {}", id);
+        } else {
+            return new MappingResults(id, matchLine.get(0).getId(), true);
+        }
+        return new MappingResults(id, "", false);
     }
 
     private static String getOrderCode(String id) {
@@ -60,7 +84,7 @@ public final class UcteMapping {
         return id.substring(0, 7);
     }
 
+    //Utility class
     private UcteMapping() {
     }
-
 }
--- src/test/java/com/rte_france/trm_algorithm/UcteMappingTest.java
+++ src/test/java/com/rte_france/trm_algorithm/UcteMappingTest.java
@@ -10,9 +10,13 @@ package com.rte_france.trm_algorithm;
 import com.powsybl.iidm.network.Line;
 import com.powsybl.iidm.network.Network;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 
 public class UcteMappingTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UcteMapping.class);
     @Test
     void testMapIdenticalLine() {
         Network networkReference = TestUtils.importNetwork("TestCase12Nodes/TestCase12Nodes.uct");
@@ -44,16 +49,43 @@ public class UcteMappingTest {
 
     @Test
     void testMultiLines() {
-        //Network networkReference = TestUtils.importNetwork("TestCase12Nodes/TestCase12Nodes_NewId.uct");
-        //Network networkMarketBased = TestUtils.importNetwork("TestCase12Nodes/TestCase12Nodes.uct");
-        Network networkReference = TestUtils.importNetwork("TestCase12Nodes/TestCase12Nodes.uct");
-        Network networkMarketBased = TestUtils.importNetwork("TestCase12Nodes/TestCase12Nodes_NewId.uct");
-        List<Line> lines = new ArrayList<>();
+        Network networkReference = TestUtils.importNetwork("TestCase12Nodes/TestCase12Nodes_NewId.uct");
+        Network networkMarketBased = TestUtils.importNetwork("TestCase12Nodes/TestCase12Nodes.uct");
+        List<Line> lines;
         lines = networkMarketBased.getLineStream().toList();
+        Map<String,String> groupedCodLines = new HashMap<>();
         for (Line line : lines) {
             MappingResults mappingResults = UcteMapping.mapNetworks(networkReference, networkMarketBased, line.getId());
             String lineId = mappingResults.lineFromReferenceNetwork();
-            System.out.println(line + "  ||  " + lineId);
+             assert mappingResults.mappingFound()==true;
+             groupedCodLines.put(lineId,line.getId());
         }
+    }
+
+    @Test
+    void testMultiLinesList() {
+        Network networkReference = TestUtils.importNetwork("TestCase12Nodes/20170322_1844_SN3_FR2.uct");
+        Network networkMarketBased = TestUtils.importNetwork("TestCase12Nodes/20170322_1844_SN3_FR2_NewId.uct");
+        List<String> linesId = new ArrayList<>();
+        networkMarketBased.getLines().forEach(line -> {
+            linesId.add(line.getId());
+        });
+        List<MappingResults> mappingResults = UcteMapping.mapNetworks(networkReference,networkMarketBased,linesId);
+        Map<String,String> groupedCodLines = new HashMap<>();
+        mappingResults.forEach(n -> {
+            groupedCodLines.put(n.lineFromMarketBasedNetwork(),n.lineFromReferenceNetwork());
+        });
+    }
+
+    @Test
+    void testLineElementName() {
+        Network networkReference = TestUtils.importNetwork("TestCase12Nodes/20170322_1844_SN3_FR2.uct");
+        Network networkMarketBased = TestUtils.importNetwork("TestCase12Nodes/20170322_1844_SN3_FR2.uct");
+        List<String> linesId = new ArrayList<>();
+        networkMarketBased.getLines().forEach(line -> {
+            linesId.add(line.getId());
+        });
+        String elementName = networkReference.getLine("FFNHV111 FFNHV211 1").getProperty("elementName");
+        System.out.println(elementName);
     }
 }
