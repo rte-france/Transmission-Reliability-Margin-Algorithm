5c4957c0b3fd75aed703435daf247f72710115d4
Viktor Terrier <viktor.terrier@rte-france.com>
2025-06-25 17:39:26 +0200

    Use existing methods

--- src/main/java/com/rte_france/trm_algorithm/operational_conditions_aligners/ItalyNorthExchangeAligner.java
+++ src/main/java/com/rte_france/trm_algorithm/operational_conditions_aligners/ItalyNorthExchangeAligner.java
@@ -7,15 +7,28 @@
  */
 package com.rte_france.trm_algorithm.operational_conditions_aligners;
 
-import com.powsybl.balances_adjustment.util.CountryAreaFactory;
+import com.farao_community.farao.dichotomy.api.exceptions.GlskLimitationException;
+import com.farao_community.farao.dichotomy.api.exceptions.ShiftingException;
+import com.farao_community.farao.dichotomy.shift.LinearScaler;
+import com.farao_community.farao.dichotomy.shift.ShiftDispatcher;
+import com.farao_community.farao.dichotomy.shift.SplittingFactors;
+import com.google.common.collect.ImmutableMap;
+import com.powsybl.glsk.commons.CountryEICode;
+import com.powsybl.glsk.commons.ZonalData;
+import com.powsybl.glsk.ucte.UcteGlskDocument;
+import com.powsybl.iidm.modification.scalable.Scalable;
 import com.powsybl.iidm.network.Country;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.loadflow.LoadFlow;
+
+import com.rte_france.trm_algorithm.TrmUtils;
+import com.rte_france.trm_algorithm.operational_conditions_aligners.exchange_and_net_position.ExchangeAndNetPosition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
 import java.util.Map;
+
+import static com.powsybl.iidm.network.Country.*;
 
 /**
  * @author Viktor Terrier {@literal <viktor.terrier at rte-france.com>}
@@ -23,30 +36,42 @@ import java.util.Map;
 public class ItalyNorthExchangeAligner implements OperationalConditionAligner {
     private static final double EXCHANGE_EPSILON = 1e-1;
     private static final Logger LOGGER = LoggerFactory.getLogger(ItalyNorthExchangeAligner.class);
-    private Map<Country, Double> countriesBalances = new HashMap<>();
 
-    public ItalyNorthExchangeAligner(Network network) {
-        countriesBalances = computeCountriesBalances(network);
-    }
-
-    private Map<Country, Double> computeCountriesBalances(Network network) {
-        LoadFlow.run(network);
-        Map<Country, Double> countriesBalances = new HashMap<>();
-        countriesBalances.put(Country.AT, new CountryAreaFactory(Country.AT).create(network).getNetPosition());
-        countriesBalances.put(Country.CH, new CountryAreaFactory(Country.CH).create(network).getNetPosition());
-        countriesBalances.put(Country.FR, new CountryAreaFactory(Country.FR).create(network).getNetPosition());
-        countriesBalances.put(Country.SI, new CountryAreaFactory(Country.SI).create(network).getNetPosition());
-        countriesBalances.put(Country.IT, new CountryAreaFactory(Country.IT).create(network).getNetPosition());
-        countriesBalances.put(Country.DE, new CountryAreaFactory(Country.DE).create(network).getNetPosition());
-        return countriesBalances;
-    }
-
-    public double getCountryBalance(Country country) {
-        return countriesBalances.get(country);
+    public ItalyNorthExchangeAligner() {
+        UcteGlskDocument ucteGlskDocument = null;
     }
 
     @Override
     public void align(Network referenceNetwork, Network marketBasedNetwork) {
         LOGGER.info("Aligning exchanges");
+
+        LoadFlow.run(referenceNetwork); // TODO: point at loadflow parameters
+        LoadFlow.run(marketBasedNetwork);
+
+        ExchangeAndNetPosition referenceExchangeAndNetPosition = new ExchangeAndNetPosition(referenceNetwork);
+        ExchangeAndNetPosition initialMarketBasedExchangeAndNetPosition = new ExchangeAndNetPosition(marketBasedNetwork);
+
+        ZonalData<Scalable> zonalScalable = TrmUtils.getAutoScalable(marketBasedNetwork);
+
+        Map<String, Double>  splittingFactors = ImmutableMap.of(
+                new CountryEICode(FR).getCode(), 0.4,
+                new CountryEICode(AT).getCode(), 0.3,
+                new CountryEICode(CH).getCode(), 0.1,
+                new CountryEICode(SI).getCode(), 0.2
+        );
+
+        ShiftDispatcher shiftDispatcher = new SplittingFactors(splittingFactors);
+
+        LinearScaler linearScaler = new LinearScaler(zonalScalable, shiftDispatcher);
+        try {
+            linearScaler.shiftNetwork(referenceExchangeAndNetPosition.getNetPosition(Country.IT), marketBasedNetwork);
+            ExchangeAndNetPosition finalMarketBasedExchangeAndNetPosition = new ExchangeAndNetPosition(marketBasedNetwork);
+
+            int i = 1;
+        } catch (GlskLimitationException e) {
+            throw new RuntimeException(e);
+        } catch (ShiftingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/com/rte_france/trm_algorithm/operational_conditions_aligners/ItalyNorthExchangeAlignerTest.java
+++ src/test/java/com/rte_france/trm_algorithm/operational_conditions_aligners/ItalyNorthExchangeAlignerTest.java
@@ -7,52 +7,23 @@
  */
 package com.rte_france.trm_algorithm.operational_conditions_aligners;
 
-import com.powsybl.iidm.network.Country;
 import com.powsybl.iidm.network.Network;
 import com.rte_france.trm_algorithm.TestUtils;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Viktor Terrier {@literal <viktor.terrier at rte-france.com>}
  */
 class ItalyNorthExchangeAlignerTest {
     public static final double EPSILON = 1e-1;
-    public static final double EPSILON_BIG = 5;
-
-    @Test
-    void testComputeItalianNetPosition() {
-        Network network = TestUtils.importNetwork("TestCase12Nodes/NETWORK_TEST_IN.uct");
-        ItalyNorthExchangeAligner italyNorthExchangeAligner = new ItalyNorthExchangeAligner(network);
-
-        assertEquals(-1948.4, italyNorthExchangeAligner.getCountryBalance(Country.IT), EPSILON);
-        assertEquals(-1950.0, italyNorthExchangeAligner.getCountryBalance(Country.IT), EPSILON_BIG);
-    }
 
     @Test
     void testComputeAllNetPositions() {
-        Network network = TestUtils.importNetwork("TestCase12Nodes/NETWORK_TEST_IN.uct");
-        ItalyNorthExchangeAligner italyNorthExchangeAligner = new ItalyNorthExchangeAligner(network);
+        Network referenceNetwork = TestUtils.importNetwork("TestCase12Nodes/NETWORK_TEST_IN_REFERENCE.uct");
+        Network marketBasedNetwork = TestUtils.importNetwork("TestCase12Nodes/NETWORK_TEST_IN.uct");
 
-        assertEquals(401.0, italyNorthExchangeAligner.getCountryBalance(Country.AT), EPSILON);
-        assertEquals(323.2, italyNorthExchangeAligner.getCountryBalance(Country.CH), EPSILON); //350
-        assertEquals(501.7, italyNorthExchangeAligner.getCountryBalance(Country.DE), EPSILON);
-        assertEquals(618.8, italyNorthExchangeAligner.getCountryBalance(Country.FR), EPSILON); //600
-        assertEquals(-1948.4, italyNorthExchangeAligner.getCountryBalance(Country.IT), EPSILON);
-        assertEquals(103.3, italyNorthExchangeAligner.getCountryBalance(Country.SI), EPSILON);
-    }
-
-    @Test
-    void testShiftNetworkToFixedItalianImport() {
-    }
-
-    @Test
-    void testShiftLoopNetworkWithEpsilon() {
-    }
-
-    @Test
-    void testShiftNetworkToOtherNetwork() {
+        ItalyNorthExchangeAligner italyNorthExchangeAligner = new ItalyNorthExchangeAligner();
+        italyNorthExchangeAligner.align(referenceNetwork, marketBasedNetwork);
     }
 
     @Test
